ec4c8a89a9218b55ccfa2c7e83e04bec34a0f737
Gloria Ho <gloria.ho@ibm.com>
2020-01-10 09:44:19 -0500

    addeding translate function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 tj_led = require('./led.js');
 tj_arm = require('./arm.js');
 tj_listen = require('./listen.js');
+tj_translate = require('./translate.js');
 
 tj_led.pulseRandom;
 tj_arm.wave;
-tj_listen.listen();
+// tj_listen.listen();
+tj_translate.translate();
--- translate.js
+++ translate.js
@@ -1,28 +1,33 @@
-var TJBot = require("tjbot");
+const TJBot = require("tjbot");
 
-var tj = new TJBot(
+let tj = new TJBot(
   [],
   {},
   {
    language_translator: {    
-    apikey: "V1A5o-PA2LW01SytinR02it3W-nOxJB22ACBMmjvW1nv"
+    apikey: "V1A5o-PA2LW01SytinR02it3W-nOxJB22ACBMmjvW1nv",
+    "url": "https://api.us-east.language-translator.watson.cloud.ibm.com/instances/c8c21cd8-2aeb-4baa-aaf4-e2b0988ead7d"
    },
   }
 );
 
-var text = "bonjour";
+let text = "bonjour";
 
-tj.identifyLanguage(text).then(function(languages) {
-  var sourceLanguage = languages.languages[0].language;
-  var targetLanguage = "en";
-  
-  tj.isTranslatable(sourceLanguage, targetLanguage).then(function(result) {
-    if(result) {
-      tj.translate(text, sourceLanguage, targetLanguage).then(function(translation) {
-        console.log(translation.translations[0].translation);
-      });
-    } else {
-      console.log("Unable to translate from '"+sourceLanguage+"' to '"+targetLanguage+"'");
+module.exports = {
+    translate: function() {
+        tj.identifyLanguage(text).then(function(languages) {
+            var sourceLanguage = languages.languages[0].language;
+            var targetLanguage = "en";
+                
+            tj.isTranslatable(sourceLanguage, targetLanguage).then(function(result) {
+                if(result) {
+                    tj.translate(text, sourceLanguage, targetLanguage).then(function(translation) {
+                        console.log(translation.translations[0].translation);
+                    });
+                } else {
+                console.log("Unable to translate from '"+sourceLanguage+"' to '"+targetLanguage+"'");
+                }
+            })
+        })
     }
-  });  
-});
+};
